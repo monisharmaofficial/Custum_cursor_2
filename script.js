1064b30f5b6fc113f2ea8e3edd0157f95f6e2a5a
monisharmaofficial <133314701+monisharmaofficial@users.noreply.github.com>
2024-07-05 15:40:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var main = document.querySelector(".main");
+var cursor = document.querySelector(".cursor");
+var image = document.querySelector(".image");
+
+main.addEventListener("mousemove", function (dets) {
+  gsap.to(cursor, {
+    x: dets.x,
+    y: dets.y,
+    duration: 0.6,
+    // ease:"back.out",
+  });
+});
+
+image.addEventListener("mouseenter", function () {
+  cursor.innerHTML = "view more";
+  gsap.to(cursor, {
+    scale: 3,
+    backgroundColor: "#ffffff8b",
+  });
+});
+
+image.addEventListener("mouseleave", function () {
+  cursor.innerHTML = "";
+  gsap.to(cursor, {
+    scale: 1,
+    backgroundColor: "#fff",
+  });
+});
